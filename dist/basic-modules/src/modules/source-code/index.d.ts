83e3c57ef2e17853fe2780ca2c8f0ef3c4019da0
chen <zzdx12345@gmail.com>
2025-04-07 15:39:17 +0800

    源代碼模式加入prettier格式化

---
+++ dist/basic-modules/src/modules/source-code/index.d.ts
@@ -0,0 +1,7 @@
+/**
+ * @description undo redo
+ * @author wangfupeng
+ */
+import { IModuleConf } from '@wangeditor-next/core';
+declare const sourceCode: Partial<IModuleConf>;
+export default sourceCode;
