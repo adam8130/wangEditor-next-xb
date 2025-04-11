250275f53eb49660e6286cbfd2ace53ed8bb5286
chen <zzdx12345@gmail.com>
2025-04-11 11:24:39 +0800

    修正表格col元素問題

---
+++ dist/basic-modules/src/modules/source-code/menu/SourceCode.d.ts
@@ -0,0 +1,20 @@
+/**
+ * @description undo menu
+ * @author wangfupeng
+ */
+import { IButtonMenu, IDomEditor } from '@wangeditor-next/core';
+declare class SourceCode implements IButtonMenu {
+    title: string;
+    iconSvg: string;
+    tag: string;
+    active: boolean;
+    getValue(_editor: IDomEditor): string | boolean;
+    isActive(_editor: IDomEditor): boolean;
+    isDisabled(editor: IDomEditor): boolean;
+    exec(editor: IDomEditor, _value: string | boolean): void;
+    private sourceCodeMode;
+    private handlePrettierCode;
+    private parseHtmlToCode;
+    private parseCodeToHtml;
+}
+export default SourceCode;
